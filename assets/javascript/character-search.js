ec1a3a880a459b737deb20d0ebb1be9a475e5dff
Luiz Mello <adao.luiz.mello@gmail.com>
2018-07-04 21:45:27 -0300

    Alterações pra consumir a API da Marvel
    - Consumir a API da Marvel;
    - Exibir os dados na tela

--- assets/javascript/character-search.js
+++ assets/javascript/character-search.js
@@ -1,22 +1,39 @@
 import axios from "axios";
 import md5 from "md5";
 
-export default name => {
+export default (name, tipo) => {
   const ts = new Date().getTime();
   const publickey = "9b3805d4e5d43aafcfd12b66e40b8b8b";
   const privatekey = "79548943cdd777e0e208e246fd743a0bff2ea039";
 
   const hash = md5(`${ts}${privatekey}${publickey}`);
-  const heroName = "Hulk";
 
   const instance = axios.create({
-    baseURL: `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${heroName}&apikey=${publickey}&hash=${hash}`,
+    baseURL: `https://gateway.marvel.com:443/v1/public/${tipo}?nameStartsWith=${name}&apikey=${publickey}&hash=${hash}`,
     timeout: 10000
   });
 
   instance
     .get()
+    /*.then(response => 
+      for (let i = 0; i < respnse.data.data.length; i++){
+        console.log(response.data.data.results[i]);
+      }*/
     //.then(response => console.log(response))
-    .then(response => console.log(response.data.data.results[0]))
+    //.then(response => console.log(response.data.data.results[0]))
+    //.then(response => document.getElementById("nameReturn").innerHTML = "Teste")
+    .then(
+      response =>
+        (document.getElementById("nameReturn").innerHTML =
+          '<img class="thumbnail" id="imgHero" src="' +
+          response.data.data.results[0].thumbnail.path +
+          "." +
+          response.data.data.results[0].thumbnail.extension +
+          '" alt = "Personagem" >' +
+          "<h1>" +
+          response.data.data.results[0].name +
+          "</h1><br>" +
+          response.data.data.results[0].description)
+    )
     .catch(error => console.log(error));
 };
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,9 +9,29 @@ form[0].addEventListener("submit", event => {
 
   const formData = new FormData(formComponent);
 
-  search(formData.get("heroName"));
+  search(formData.get("heroName"), "characters");
 
   //console.log(formData);
 });
 
-//console.log(form[0]);
+form[1].addEventListener("submit", event => {
+  event.preventDefault();
+
+  const formComponent = event.target;
+
+  const formData = new FormData(formComponent);
+
+  search(formData.get("eventName"), "events");
+});
+
+form[2].addEventListener("change", event => {
+  event.preventDefault();
+
+  const formComponent = event.target;
+
+  const formData = new FormData(formComponent);
+
+  formData.append("Teste");
+
+  //search(formData.get("nameReturn"), "events");
+});
